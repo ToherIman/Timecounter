39358c69ccc1c0ad30104dd88450a6f2af106a4d
ToherIman <e2h3in@gmail.com>
2017-06-15 19:28:49 +0200

    Timer and counter

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: 0,
+      count: 0
+      };
+
+  }
+  handleClick = () => {
+    this.setState({count: this.state.count + 1 });
+  };
+  start = () => {
+    this.setState({date: new Date()});
+  }
+
+  render() {
+    const buttonStyle = {
+      width: '100%',
+      height: 'auto',
+      backgroundColor: 'green',
+      border: 'none',
+      fontSize: '18em'
+    };
+    const counterStyle = {
+      width: '100%',
+      height: 'auto',
+      backgroundColor: 'blue',
+      border: 'none',
+      fontSize: '2em'
+    };
+    return (
+      <div className="App">
+        <div className="App-header">
+
+          <h2>Timer&Counter</h2>
+        </div>
+
+      <div style={counterStyle}>{Date.now()-this.state.date}</div>
+      <button onClick={this.handleClick} style={buttonStyle}>{this.state.count}</button>
+      <button onClick={this.start} style={buttonStyle}>Start</button>
+      </div>
+    );
+  }
+}
+
+export default App;
