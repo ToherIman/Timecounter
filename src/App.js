6ef08eaca1047b06e31bd3567a2aea0518ed10c5
ToherIman <e2h3in@gmail.com>
2017-06-16 21:50:27 +0200

    Formating

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,14 +8,17 @@ class App extends Component {
     this.state = {
       date: 0,
       count: 0
-      };
+    };
 
-  }
+  };
   handleClick = () => {
-    this.setState({count: this.state.count + 1 });
+    this.setState({
+      count: this.state.count + 1
+    });
   };
   start = () => {
     this.setState({date: new Date()});
+    this.setState({count: 0});
   }
 
   render() {
@@ -34,17 +37,15 @@ class App extends Component {
       fontSize: '2em'
     };
     return (
-      <div className="App">
-        <div className="App-header">
-
-          <h2>Timer&Counter</h2>
-        </div>
-
-      <div style={counterStyle}>{Date.now()-this.state.date}</div>
-      <button onClick={this.handleClick} style={buttonStyle}>{this.state.count}</button>
-      <button onClick={this.start} style={buttonStyle}>Start</button>
-      </div>
-    );
+      <div className = "App" >
+          <div className = "App-header" >
+            <h2> Timer & Counter </h2>
+          </div >
+        <div style = {counterStyle} > {Date.now() - this.state.date} </div>
+        <button onClick = { this.handleClick } style = { buttonStyle } > { this.state.count } </button>
+        <button onClick = {this.start} style = {buttonStyle}> Start </button>
+      </div >
+      );
   }
 }
 
